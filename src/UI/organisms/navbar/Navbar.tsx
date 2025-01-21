36736a05926b453c84a5d22d4c1ee8ecf59f4638
sebas1913 <sebasosornicastrillon@gmail.com>
2025-01-20 20:57:49 -0500

    Update navbar component

--- src/UI/organisms/navbar/Navbar.tsx
+++ src/UI/organisms/navbar/Navbar.tsx
@@ -2,16 +2,13 @@ import Title from '@/UI/atoms/title/Title';
 import styles from './navbar.module.scss';
 import { Icons } from '@/UI/atoms/icons/Icons';
 import Link from 'next/link';
-// import Image from 'next/image';
 
 const Navbar: React.FC = () => {
     return (
         <nav className={styles.navbar}>
-            <div className={styles.title}>
-                {/* <Image src="/images/logo.png" width={50} height={40} alt="Logo SIEK" /> */}
+            <Link href={'/'} className={styles.title}>
                 <Title level={1}>siekGPT</Title>
-
-            </div>
+            </Link>
             <div className={styles.icon}>
                 <Link className={styles.link} href={'/history'}>{Icons.history}</Link>
             </div>
